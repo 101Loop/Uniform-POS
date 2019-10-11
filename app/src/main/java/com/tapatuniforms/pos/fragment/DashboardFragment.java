4c7584401aad58bae7cbd2807a80013330a0f3f1
dipanshu <dipanshu.singh1997@gmail.com>
2019-10-11 19:04:22 +0530

    Implement Pie chart and Bar chart

--- app/src/main/java/com/tapatuniforms/pos/fragment/DashboardFragment.java
+++ app/src/main/java/com/tapatuniforms/pos/fragment/DashboardFragment.java
@@ -1,34 +1,66 @@
 package com.tapatuniforms.pos.fragment;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.DefaultValueFormatter;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.formatter.StackedValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.model.GradientColor;
+import com.github.mikephil.charting.utils.MPPointF;
 import com.tapatuniforms.pos.R;
-import com.tapatuniforms.pos.activity.PosActivity;
 import com.tapatuniforms.pos.adapter.DashboardAdapter;
 import com.tapatuniforms.pos.helper.DatabaseHelper;
 import com.tapatuniforms.pos.helper.DatabaseSingleton;
 import com.tapatuniforms.pos.helper.GridItemDecoration;
 import com.tapatuniforms.pos.model.DashboardItem;
+import com.tapatuniforms.pos.model.PieChartItem;
 import com.tapatuniforms.pos.model.ProductHeader;
 import com.tapatuniforms.pos.network.ProductAPI;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
-public class DashboardFragment extends Fragment implements DashboardAdapter.OnClickListener {
+public class DashboardFragment extends Fragment implements DashboardAdapter.OnClickListener, OnChartValueSelectedListener {
     private RecyclerView dashRecycler;
     private DashboardAdapter dashAdapter;
     private DatabaseSingleton db;
     private ArrayList<ProductHeader> productList;
     private ArrayList<ProductHeader> allProductList;
+    private PieChart pieChart;
+    private BarChart barChart;
+    private ArrayList<PieEntry> entries;
+    private ArrayList<PieChartItem> pieChartItems;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,11 +81,109 @@ public class DashboardFragment extends Fragment implements DashboardAdapter.OnCl
         dashRecycler.setAdapter(dashAdapter);
         dashAdapter.setOnClickListener(this);
 
+        entries = new ArrayList<>();
+        pieChartItems = new ArrayList<>();
+        getEntries();
+
+        pieChart = v.findViewById(R.id.pieChart);
+        barChart = v.findViewById(R.id.barChart);
+
+        initPieChart();
+        initBarChart();
+
         db = DatabaseHelper.getDatabase(getContext());
         productList = new ArrayList<>();
         allProductList = new ArrayList<>();
 
         ProductAPI.fetchProducts(getContext(), allProductList, productList, null, null, db, null, null);
+    }
+
+    private void initBarChart() {
+        barChart.setOnChartValueSelectedListener(this);
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.getDescription().setEnabled(false);
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        barChart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        barChart.setPinchZoom(true);
+
+        barChart.setDrawGridBackground(false);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(5f);
+        xAxis.setLabelCount(5);
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setLabelCount(4, false);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis = barChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setLabelCount(0, true);
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        Legend l1 = barChart.getLegend();
+        l1.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l1.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l1.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l1.setDrawInside(false);
+        l1.setForm(Legend.LegendForm.SQUARE);
+        l1.setFormSize(9f);
+        l1.setTextSize(11f);
+        l1.setXEntrySpace(4f);
+
+        setBarChartData(20, 100);
+    }
+
+    private void initPieChart() {
+        pieChart.setUsePercentValues(true);
+        pieChart.setCenterText("Total Sale");
+        pieChart.animateY(1400, Easing.EaseInOutQuad);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+
+        pieChart.setTransparentCircleColor(Color.WHITE);
+        pieChart.setTransparentCircleAlpha(110);
+
+        pieChart.setHoleRadius(58f);
+        pieChart.setTransparentCircleRadius(61f);
+
+        pieChart.setDrawCenterText(true);
+
+        pieChart.setRotationAngle(0);
+        pieChart.setRotationEnabled(true);
+        pieChart.setHighlightPerTapEnabled(true);
+
+        pieChart.setOnChartValueSelectedListener(this);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
+
+        // entry label styling
+        pieChart.setEntryLabelColor(Color.WHITE);
+        pieChart.setEntryLabelTextSize(12f);
+
+        setData();
     }
 
     private ArrayList<DashboardItem> getDashboardItems() {
@@ -63,7 +193,7 @@ public class DashboardFragment extends Fragment implements DashboardAdapter.OnCl
 //        itemList.add(new DashboardItem(R.drawable.ic_box, "Order History", R.id.orderScreen));
 //        itemList.add(new DashboardItem(R.drawable.ic_diagram, "Reports", R.id.saleScreen));
         itemList.add(new DashboardItem(R.drawable.ic_inventory, "Stock Entry", R.id.stockScreen));
-        itemList.add(new DashboardItem(R.drawable.ic_storage, "Inventory",R.id.inventoryScreen));
+        itemList.add(new DashboardItem(R.drawable.ic_storage, "Inventory", R.id.inventoryScreen));
 
         return itemList;
     }
@@ -95,6 +225,168 @@ public class DashboardFragment extends Fragment implements DashboardAdapter.OnCl
             assert fragmentManager != null;
             fragmentManager.beginTransaction()
                     .replace(R.id.fragmentContainer, fragment).commit();
+        }
+    }
+
+    private void setData() {
+        PieDataSet dataSet = new PieDataSet(entries, "Total Sale");
+
+        dataSet.setSelectionShift(10f);
+        dataSet.setSliceSpace(5f);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        for (PieChartItem pieChartItem: pieChartItems) {
+            colors.add(Color.parseColor(pieChartItem.getColor()));
+        }
+
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(false);
+        pieChart.setData(data);
+
+        // undo all highlights
+        pieChart.highlightValues(null);
+
+        pieChart.invalidate();
+    }
+
+    private void setBarChartData(int count, int range) {
+        float groupSpace = 0.08f;
+        float barSpace = 0.03f; // x4 DataSet
+        float barWidth = 0.2f; // x4 DataSet
+        // (0.2 + 0.03) * 4 + 0.08 = 1.00 -> interval per "group"
+
+        int groupCount = 10;
+        int startYear = 1980;
+        int endYear = startYear + groupCount;
+
+        float start = 1f;
+
+        ArrayList<BarEntry> values1 = new ArrayList<>();
+        ArrayList<BarEntry> values2 = new ArrayList<>();
+
+        for (int i = (int) start; i < start + count; i++) {
+            float val1 = (float) (Math.random() * (range + 1));
+            float val2 = (float) (Math.random() * (range + 1));
+
+            if (Math.random() * 100 < 25) {
+                values1.add(new BarEntry(i, val1));
+                values2.add(new BarEntry(i, val2));
+            } else {
+                values1.add(new BarEntry(i, val1));
+                values2.add(new BarEntry(i, val2));
+            }
+        }
+
+        BarDataSet set1, set2;
+
+        if (barChart.getData() != null &&
+                barChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) barChart.getData().getDataSetByIndex(0);
+            set2 = (BarDataSet) barChart.getData().getDataSetByIndex(1);
+            set1.setValues(values1);
+            set2.setValues(values2);
+
+            barChart.getData().notifyDataChanged();
+            barChart.notifyDataSetChanged();
+
+        } else {
+            set1 = new BarDataSet(values1, "Order Received");
+            set1.setDrawIcons(false);
+
+            set2 = new BarDataSet(values2, "Order Not Received");
+            set2.setDrawIcons(false);
+
+            int startColor1 = 0;
+            int startColor2 = 0;
+            int startColor3 = 0;
+            int startColor4 = 0;
+            int startColor5 = 0;
+            int endColor1 = 0;
+            int endColor2 = 0;
+            int endColor3 = 0;
+            int endColor4 = 0;
+            int endColor5 = 0;
+
+            Context context = getContext();
+            if (context != null) {
+                startColor1 = ContextCompat.getColor(context, android.R.color.holo_orange_light);
+                startColor2 = ContextCompat.getColor(context, android.R.color.holo_blue_light);
+                startColor3 = ContextCompat.getColor(context, android.R.color.holo_orange_light);
+                startColor4 = ContextCompat.getColor(context, android.R.color.holo_green_light);
+                startColor5 = ContextCompat.getColor(context, android.R.color.holo_red_light);
+                endColor1 = ContextCompat.getColor(context, android.R.color.holo_blue_dark);
+                endColor2 = ContextCompat.getColor(context, android.R.color.holo_purple);
+                endColor3 = ContextCompat.getColor(context, android.R.color.holo_green_dark);
+                endColor4 = ContextCompat.getColor(context, android.R.color.holo_red_dark);
+                endColor5 = ContextCompat.getColor(context, android.R.color.holo_orange_dark);
+            }
+
+            List<GradientColor> gradientColors = new ArrayList<>();
+            gradientColors.add(new GradientColor(startColor1, endColor1));
+            gradientColors.add(new GradientColor(startColor2, endColor2));
+            gradientColors.add(new GradientColor(startColor3, endColor3));
+            gradientColors.add(new GradientColor(startColor4, endColor4));
+            gradientColors.add(new GradientColor(startColor5, endColor5));
+
+            set1.setGradientColors(gradientColors);
+            set2.setGradientColors(gradientColors);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+            dataSets.add(set2);
+
+            BarData data = new BarData(dataSets);
+            data.setValueFormatter(new LargeValueFormatter());
+            data.setValueTextSize(10f);
+            data.setBarWidth(0.5f);
+//            barChart.getXAxis().setAxisMaximum(startYear + barChart.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
+//            barChart.groupBars(startYear, groupSpace, barSpace);
+            barChart.setData(data);
+        }
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        pieChart.setCenterTextSize(18f);
+
+        PieEntry entry = (PieEntry) e.getData();
+        String color = null;
+
+        for (int i=0; i<entries.size(); i++){
+            if (entries.get(i).equals(entry)) {
+                color = pieChartItems.get(i).getColor();
+            }
+        }
+
+        if (color != null) {
+            pieChart.setCenterTextColor(Color.parseColor(color));
+        }
+
+        pieChart.setCenterText(String.valueOf(e.getY()));
+    }
+
+    @Override
+    public void onNothingSelected() {
+    }
+
+    private void getEntries(){
+        //TODO: fetch entries from the server
+
+        PieChartItem pieChartItem1 = new PieChartItem(25,"Stock left", "#49B5E8");
+        PieChartItem pieChartItem2 = new PieChartItem(20,"Sold Out", "#32AD59");
+        PieChartItem pieChartItem3 = new PieChartItem(40,"Not Sold", "#f35352");
+        PieChartItem pieChartItem4 = new PieChartItem(15,"Return", "#F7C21C");
+
+        pieChartItems.add(pieChartItem1);
+        pieChartItems.add(pieChartItem2);
+        pieChartItems.add(pieChartItem3);
+        pieChartItems.add(pieChartItem4);
+
+        for (PieChartItem pieChartItem : pieChartItems) {
+            entries.add(pieChartItem.getPieEntry());
         }
     }
 }
